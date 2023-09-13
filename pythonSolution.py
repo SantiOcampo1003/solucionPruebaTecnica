3c651196751e1816c1611d4de2760c14b41428c8
Santi-oca1003 <santi.720001@gmail.com>
2023-09-13 15:24:23 -0500

    solution commit

---
+++ pythonSolution.py
@@ -0,0 +1,10 @@
+def sort_list(input_list):
+    print(input_list)
+    for i in range(len(input_list)):
+        for j in range(len(input_list) - 1):
+            if input_list[j] > input_list[j + 1]:
+                input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
+    print(input_list)
+    return input_list
+
+sort_list([5, 2, 9, 1, 6])
